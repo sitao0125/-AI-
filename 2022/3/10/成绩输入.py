79c9d0d7eaf30e8ed60d4f7d1281b715160c8304
chensitao <sitao0125@163.com>
2023-03-10 10:50:49 +0800

    成绩输入

---
+++ 2022/3/10/成绩输入.py
@@ -0,0 +1,12 @@
+a=float(input("请输入成绩"))
+if a >90:
+    print("成绩为优秀")
+else:
+    if a > 80:
+        print("成绩良好")
+    else :
+        if a >70:
+            print("成绩中等")
+        else:
+            if a < 60:
+                print("成绩不合格")
